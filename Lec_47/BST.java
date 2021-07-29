81da3b3312cdf12263ccb64017f1524b06b15724
Lakshya-CB <78645656+Lakshya-CB@users.noreply.github.com>
2021-07-29 14:32:40 +0530

    Add files via upload

--- Lec_47/BST.java
+++ Lec_47/BST.java
@@ -171,7 +171,6 @@ public class BST {
 
 	private void add2(Node Curr, Node Parent, int ele) {
 		if (Curr == null) {
-
 			Node nn = new Node();
 			nn.data = ele;
 
@@ -188,20 +187,184 @@ public class BST {
 			add2(Curr.right, Curr, ele);
 		}
 	}
-	
-	private Node add3 (Node Curr, int ele) {
-		if(Curr==null) {
+
+	public void add3(int ele) {
+		root = add3(root, ele);
+	}
+
+	private Node add3(Node Curr, int ele) {
+		if (Curr == null) {
 			Node nn = new Node();
 			nn.data = ele;
 			return nn;
 		}
-		
-		if(Curr.data<=ele) {
+
+		if (ele <= Curr.data) {
 			Curr.left = add3(Curr.left, ele);
-		}
-		else {
-			Curr.right= add3(Curr.right, ele);	
+		} else {
+			Curr.right = add3(Curr.right, ele);
 		}
 		return Curr;
 	}
+
+	public void remove(int item) {
+		if (item == root.data) {
+			if (root.left == null && root.right == null) {
+				root = null;
+			} else if (root.left != null && root.right == null) {
+				root = root.left;
+			} else if (root.left == null && root.right != null) {
+				root = root.right;
+			}
+		} else {
+			remove(root, null, item);
+		}
+	}
+
+	private void remove(Node Curr, Node Parent, int item) {
+
+		if (item < Curr.data) {
+			remove(Curr.left, Curr, item);
+		} else if (item > Curr.data) {
+			remove(Curr.right, Curr, item);
+		} else {
+//			Curr.data === item
+//			This is the Node to remove
+
+//			Case 1: Curr Node is a Leaf Node
+			if (Curr.left == null && Curr.right == null) {
+//				 Which connection from Parent to Curr
+//				if(Curr.data<Parent.data) {
+				if (Parent.left.data == item) {
+					Parent.left = null;
+				} else {
+					Parent.right = null;
+				}
+			}
+//			Case 2 : When your Curr Node has a Single Child!
+
+//			Case 2 a : When Your Curr Node have left 
+			else if (Curr.left != null && Curr.right == null) {
+				if (Parent.left.data == Curr.data) {
+					Parent.left = Curr.left;
+				} else {
+					Parent.right = Curr.left;
+				}
+			}
+//			Case 2 b : When Your Curr Node have right 
+			else if (Curr.left == null && Curr.right != null) {
+				if (Parent.left.data == Curr.data) {
+					Parent.left = Curr.right;
+				} else {
+					Parent.right = Curr.right;
+				}
+			}
+//			Case 3 : When your Curr Node has both children  
+//			I actually Updated the Node data
+			else {
+				int Max_LST = Max(Curr.left);
+				Curr.data = Max_LST;
+				remove(Curr.left, Curr, Max_LST);
+			}
+		}
+	}
+
+	public void remove3(int item) {
+		root = remove3(root, item);
+
+	}
+
+	private Node remove3(Node Curr, int item) {
+		if (item < Curr.data) {
+			Curr.left = remove3(Curr.left, item);
+
+		} else if (item > Curr.data) {
+			Curr.right = remove3(Curr.right, item);
+
+		} else {
+//			Curr is the Node that you have to remove
+			if (Curr.left == null && Curr.right == null) {
+				return null;
+			}
+//			Case 2 : When your Curr Node has a Single Child!
+
+//			Case 2 a : When Your Curr Node have left 
+			else if (Curr.left != null && Curr.right == null) {
+				return Curr.left;
+
+			}
+//			Case 2 b : When Your Curr Node have right 
+			else if (Curr.left == null && Curr.right != null) {
+				return Curr.right;
+			}
+//			Case 3 : When your Curr Node has both children  
+//			I actually Updated the Node data
+			else {
+				int Max_LST = Max(Curr.left);
+				Curr.data = Max_LST;
+				Curr.left = remove3(Curr.left, Max_LST);
+				return Curr;
+			}
+		}
+		return Curr;
+	}
+
+	public boolean isBST() {
+//		return isBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+		return isBST2(root).isBST;
+	}
+
+	private boolean isBST(Node curr, int min, int max) {
+		if (curr == null)
+			return true;
+		if (curr.data < min || curr.data > max)
+			return false;
+		else {
+			boolean flag = isBST(curr.left, min, curr.data);
+			if (flag == false)
+				return flag;
+			return isBST(curr.right, curr.data, max);
+		}
+	}
+
+	private class Mx_Mn_isBST {
+		int Max = Integer.MIN_VALUE;
+		int Min = Integer.MAX_VALUE;
+		int Largest_Size_BST = 0;
+		int Largest_BST_root_val = 0;
+		boolean isBST = true;
+	}
+
+	private Mx_Mn_isBST isBST2(Node Curr) {
+		if (Curr == null) {
+
+			return new Mx_Mn_isBST();
+		}
+
+		Mx_Mn_isBST left = isBST2(Curr.left);
+		Mx_Mn_isBST right = isBST2(Curr.right);
+
+		Mx_Mn_isBST ans = new Mx_Mn_isBST();
+
+		if (Curr.data >= left.Max && Curr.data < right.Min && left.isBST && right.isBST) {
+			ans.isBST = true;
+			ans.Largest_Size_BST = left.Largest_Size_BST + right.Largest_Size_BST + 1;
+			ans.Largest_BST_root_val = Curr.data;
+		} else {
+			ans.isBST = false;
+			if (left.Largest_Size_BST > right.Largest_Size_BST) {
+				ans.Largest_Size_BST = left.Largest_Size_BST;
+				ans.Largest_BST_root_val = left.Largest_BST_root_val;
+			} else {
+				ans.Largest_Size_BST = right.Largest_Size_BST;
+				ans.Largest_BST_root_val = right.Largest_BST_root_val;
+			}
+		}
+		ans.Max = Math.max(Math.max(left.Max, right.Max), Curr.data);
+		ans.Min = Math.min(Math.min(left.Min, right.Min), Curr.data);
+		System.out.println(Curr.data + " ; Max = " + ans.Max + " ; Min = " + ans.Min);
+		return ans;
+
+	}
+
 }
--- Lec_47/BST_Client.java
+++ Lec_47/BST_Client.java
@@ -2,25 +2,46 @@ package Lec_47;
 
 public class BST_Client {
 	public static void main(String[] args) {
-		int[] in = {  };
+		int[] in = {10,20,30,40,50,60,70};
 		BST TT = new BST(in);
-		TT.Disp();
+//		TT.Disp();
 //		System.out.println(TT.Max());
 //		System.out.println(TT.find(20));
 //		System.out.println(TT.find(25));
 //		System.out.println(TT.find(35));
 //		System.out.println(TT.find(50));
-		System.out.println("--------------");
+//		System.out.println("--------------");
 //		TT.Print_bw(15, 65);
 //		TT.replace_Greater();
 //		TT.Disp();
-		TT.add2(10);
-		TT.add2(10);
-		TT.add2(10);
-		TT.add2(10);
-		TT.add2(10);
-		
+//		TT.add2(10);
+//		TT.add2(10);
+//		TT.add2(10);
+//		TT.add2(10);
+//		TT.add2(10);
+//		
+//		TT.Disp();
+//		TT.add2(55);
+//		TT.add2(52);
+//		TT.add2(57);
+//		System.out.println("--------------");
+//		TT.Disp();
+//		System.out.println("--------------60");
+//		TT.remove3(60);
+//		TT.Disp();
+//		
+//		System.out.println("--------------52");
+//		TT.remove3(52);
+//		TT.Disp();
+//		
+//		System.out.println("--------------50");
+//		TT.remove3(50);
+//		TT.Disp();
+
+//		System.out.println("--------------50");
+//		TT.remove3(10);
 		TT.Disp();
 
+		System.out.println(TT.isBST());
 	}
 }
