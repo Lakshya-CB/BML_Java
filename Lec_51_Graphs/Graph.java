5195d325442acdb138bab51546ca476e84118412
Lakshya-CB <78645656+Lakshya-CB@users.noreply.github.com>
2021-08-10 23:57:37 +0530

    Add files via upload

--- Lec_51_Graphs/Graph.java
+++ Lec_51_Graphs/Graph.java
@@ -1,9 +1,12 @@
 package Lec_51_Graphs;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 import Lec_42_43.Linked_List;
 
@@ -85,11 +88,11 @@ public class Graph {
 	}
 
 	public boolean BFS(int src, int dest) {
-		HashSet<Integer> Visited = new HashSet<>();
+		HashSet<Integer> isPresentQ = new HashSet<>();
 		Queue<Integer> Q = new LinkedList();
 
 		Q.add(src);
-		Visited.add(src);
+		isPresentQ.add(src);
 		String str = "";
 		while (!Q.isEmpty()) {
 			int V = Q.poll();
@@ -97,37 +100,320 @@ public class Graph {
 			if (V == dest) {
 				return true;
 			}
+
 			for (int nbr : map.get(V).keySet()) {
-				if (!Visited.contains(nbr)) {
+				if (!isPresentQ.contains(nbr)) {
 					Q.add(nbr);
-					Visited.add(nbr);
+					isPresentQ.add(nbr);
 				}
 			}
 
 		}
 		return false;
 	}
+
 	public boolean DFS(int src, int dest) {
-		HashSet<Integer> Visited = new HashSet<>();
+		HashSet<Integer> isPresentS = new HashSet<>();
 		Stack<Integer> Q = new Stack();
 
 		Q.add(src);
-		Visited.add(src);
+		isPresentS.add(src);
 		String str = "";
 		while (!Q.isEmpty()) {
-			int V = Q.poll();
+			int V = Q.pop();
 //			System.out.print(V + " ");
 			if (V == dest) {
 				return true;
 			}
 			for (int nbr : map.get(V).keySet()) {
-				if (!Visited.contains(nbr)) {
+				if (!isPresentS.contains(nbr)) {
 					Q.add(nbr);
-					Visited.add(nbr);
+					isPresentS.add(nbr);
 				}
 			}
 
 		}
 		return false;
 	}
+
+	public void BFT() {
+		HashSet<Integer> isPresentQ = new HashSet<>();
+		Queue<Integer> Q = new LinkedList();
+
+//		int src = 1; 
+		for (int src : map.keySet()) {
+			if (isPresentQ.contains(src)) {
+				continue;
+			}
+
+			Q.add(src);
+
+			isPresentQ.add(src);
+
+			while (!Q.isEmpty()) {
+				int V = Q.poll();
+				System.out.print(V + " ");
+
+				for (int nbr : map.get(V).keySet()) {
+					if (!isPresentQ.contains(nbr)) {
+						Q.add(nbr);
+						isPresentQ.add(nbr);
+					}
+				}
+
+			}
+		}
+	}
+
+	public void BFT2() {
+		HashSet<Integer> Visited = new HashSet<>();
+		Queue<Integer> Q = new LinkedList();
+
+		for (int src : map.keySet()) {
+			if (Visited.contains(src)) {
+				continue;
+			}
+			Q.add(src);
+
+			while (!Q.isEmpty()) {
+
+				if (Visited.contains(Q.peek())) {
+					Q.poll();
+					continue;
+				}
+
+				int V = Q.poll();
+				System.out.print(V + " ");
+				Visited.add(V);
+
+				for (int nbr : map.get(V).keySet()) {
+					if (!Visited.contains(nbr)) {
+						Q.add(nbr);
+					}
+				}
+
+			}
+		}
+		return;
+	}
+
+	public int numOfComponents() {
+		HashSet<Integer> isPresentQ = new HashSet<>();
+		Queue<Integer> Q = new LinkedList();
+		int components = 0;
+//		int src = 1; 
+		for (int src : map.keySet()) {
+			if (isPresentQ.contains(src)) {
+				continue;
+			}
+			components++;
+			Q.add(src);
+			isPresentQ.add(src);
+			while (!Q.isEmpty()) {
+				int V = Q.poll();
+				System.out.print(V + " ");
+
+				for (int nbr : map.get(V).keySet()) {
+					if (!isPresentQ.contains(nbr)) {
+						Q.add(nbr);
+						isPresentQ.add(nbr);
+					}
+				}
+			}
+			System.out.println("----------------------");
+
+		}
+		return components;
+	}
+
+	public boolean isCyclic() {
+		HashSet<Integer> Visited = new HashSet<>();
+		Queue<Integer> Q = new LinkedList();
+
+		for (int src : map.keySet()) {
+			if (Visited.contains(src)) {
+				continue;
+			}
+			Q.add(src);
+
+			while (!Q.isEmpty()) {
+				if (Visited.contains(Q.peek())) {
+					return true;
+				}
+				int V = Q.poll();
+				System.out.print(V + " ");
+				Visited.add(V);
+
+				for (int nbr : map.get(V).keySet()) {
+					if (!Visited.contains(nbr)) {
+						Q.add(nbr);
+					}
+				}
+
+			}
+		}
+		return false;
+	}
+
+	public boolean isTree() {
+		return numOfComponents() == 1 && !isCyclic();
+	}
+
+	public void getConnectedCompo() {
+		HashSet<Integer> isPresentQ = new HashSet<>();
+		ArrayList<ArrayList<Integer>> All_comp = new ArrayList<>();
+		Queue<Integer> Q = new LinkedList();
+		int components = 0;
+
+		for (int src : map.keySet()) {
+			if (isPresentQ.contains(src)) {
+				continue;
+			}
+			components++;
+			All_comp.add(new ArrayList<>());
+			Q.add(src);
+			isPresentQ.add(src);
+			while (!Q.isEmpty()) {
+				int V = Q.poll();
+//					System.out.print(V + " ");
+				All_comp.get(All_comp.size() - 1).add(V);
+				for (int nbr : map.get(V).keySet()) {
+					if (!isPresentQ.contains(nbr)) {
+						Q.add(nbr);
+						isPresentQ.add(nbr);
+					}
+				}
+			}
+//				System.out.println("----------------------");
+
+		}
+		System.out.println(All_comp);
+
+	}
+
+	class Disjoint {
+		class TreeNode {
+			int data;
+			TreeNode parent;
+			int rank;
+		}
+
+		HashMap<Integer, TreeNode> mapping = new HashMap<>();
+
+//		O(V)
+		public void create(int numV) {
+			for (int i = 1; i <= numV; i++) {
+				TreeNode n = new TreeNode();
+				n.data = i;
+				n.parent = n;
+				n.rank = 0;
+				mapping.put(i, n);
+			}
+		}
+
+// O(ht) to O(1)
+		public int find(int V0) {
+//			Form the hashmap i got 2k
+			TreeNode n = mapping.get(V0);
+			return find(n).data;
+		}
+
+//		Will find the root node of my Tree set
+		private TreeNode find(TreeNode n) {
+			if (n.parent == n) {
+				return n;
+			}
+			TreeNode rr = find(n.parent);
+			n.parent = rr; // Path suppression for converting from O(ht) to Avg O(1)
+			return rr;
+		}
+
+		public void Merge(int V1, int V2) {
+
+			int RV1 = find(V1);
+			int RV2 = find(V2);
+
+//			O(1)
+
+//			If Cycle Do not merge
+			if (RV1 == RV2) {
+				return;
+			}
+
+			TreeNode N1 = mapping.get(RV1);
+			TreeNode N2 = mapping.get(RV2);
+//			Case 1 : rank are same
+			if (N1.rank == N2.rank) {
+				N2.parent = N1;
+				N1.rank++;
+			} else if (N1.rank > N2.rank) {
+				N2.parent = N1;
+			} else {
+				N1.parent = N2;
+			}
+
+		}
+
+	}
+
+	class EdgePair implements Comparable<EdgePair> {
+		int Wt;
+		int V1;
+		int V2;
+
+		public EdgePair(int w, int v1, int v2) {
+			// TODO Auto-generated constructor stub
+			Wt = w;
+			V1 = v1;
+			V2 = v2;
+		}
+		@Override
+		public int compareTo(EdgePair o) {
+			// TODO Auto-generated method stub
+//			this - o;
+			return this.Wt - o.Wt;
+		}
+		public String toString() {
+			return " { " +V1+"-"+V2+"@"+Wt+" } "; 
+		}
+	}
+	
+	public ArrayList<EdgePair> getAllEdges(){
+		ArrayList<EdgePair> Al = new ArrayList<Graph.EdgePair>();
+		for(int V1 : map.keySet()) {
+//			All the nbr of V1
+			for(int V2 : map.get(V1).keySet()) {
+				int Wt = map.get(V1).get(V2);
+				EdgePair P = new EdgePair(Wt, V1, V2);
+				Al.add(P);
+			}
+		}
+		return Al;
+	}
+	public void Kruskal() {
+		ArrayList<EdgePair> Edges = getAllEdges();
+		Collections.sort(Edges);
+//		System.out.println(Edges);
+		
+		Disjoint Ds = new Disjoint();
+		Ds.create(map.size());
+		
+		for(EdgePair E: Edges) {
+			int V1 = E.V1;
+			int V2 = E.V2;
+			
+			int RV1 = Ds.find(V1);
+			int RV2 = Ds.find(V2);
+			
+			if(RV1==RV2) {
+//				Same set!! Ignore!!
+				continue;
+			}
+			else{
+				System.out.println(E);
+				Ds.Merge(RV1, RV2);
+			}
+		}
+	}
 }
+
--- Lec_51_Graphs/Graph_Client.java
+++ Lec_51_Graphs/Graph_Client.java
@@ -6,13 +6,13 @@ public class Graph_Client {
 	public static void main(String[] args) {
 		Graph G = new Graph(7);
 		G.addEdge(1, 2, 10);
-		G.addEdge(2, 3, 15);
-		G.addEdge(1, 4, 7);
-		G.addEdge(3, 4, 20);
-		G.addEdge(4, 5, 100);
-		G.addEdge(5, 6, 11);
-		G.addEdge(5, 7, 15);
-		G.addEdge(6, 7, 30);
+		G.addEdge(2, 3, 2);	
+		G.addEdge(1, 4, 6);
+		G.addEdge(3, 4, 3);
+		G.addEdge(4, 5, 7);
+		G.addEdge(5, 6, 9);
+		G.addEdge(5, 7, 14);
+		G.addEdge(6, 7, 13);
 //		G.Disp();
 
 //		System.out.println(G.hasPath(2, 6, new HashSet<>(),""));
@@ -22,6 +22,13 @@ public class Graph_Client {
 //		System.out.println(G.hasPath(2, 6, new HashSet<>(),""));
 
 //		G.AllPath(1, 6, new HashSet<>(), "");
-		G.BFS(1, 6);
+//		G.BFS(1, 6);
+//		G.BFT();
+//		System.out.println(G.numOfComponents());
+//		System.out.println(G.isCyclic());
+//		G.BFT2();
+//		System.out.println(G.isTree());
+//		G.getConnectedCompo();
+		G.Kruskal();
 	}
 }
